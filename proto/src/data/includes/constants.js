1e487da4bff751edbc461b5196e8d34881b1e1a9
Alejandro André <alejandro.andre@ciberexperis.es>
2023-05-18 12:36:16 +0200

    Start develop

---
+++ proto/src/data/includes/constants.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+  SERVER: "https://experis.flows.ninja",
+  HEADER: { 'Content-Type': 'application/json' },
+  LOGIN:  "mutation{login(username:\"modelsadmin\",password:\"Ciber$2022\")}"
+});
+
