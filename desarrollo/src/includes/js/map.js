d4676320f226c5eb6818c8d9877301d3936687f0
Alejandro André <alejandro.andre@ciberexperis.es>
2023-05-30 14:29:36 +0200

    Fix map

--- desarrollo/src/includes/js/map.js
+++ desarrollo/src/includes/js/map.js
@@ -1,13 +1,32 @@
 function createMap(div, lat, lon, zoom) {
+
+  // Crea el mapa
   var map = L.map(div).setView([lat, lon], zoom);
+
+  // Openstreetmap
   L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
     maxZoom: 18,
   }).addTo(map);
+
+  // Desactivar zoom con la rueda del ratón durante el scroll
+  map.scrollWheelZoom.disable();
+  map.on('blur', function () {
+    map.scrollWheelZoom.disable();
+  });  
+
+  // Reactivar zoom con la rueda del ratón cuando no se hace scroll
+  map.on('focus', function () {
+    map.scrollWheelZoom.enable();
+  });
+
+  // Return
   return map;
 }
 
 function putMarker(map, lat, lon) {
+
+  // Custom icon
   var LeafIcon = L.Icon.extend({
     options: {
       iconSize:     [38, 44],
@@ -15,6 +34,8 @@ function putMarker(map, lat, lon) {
       popupAnchor:  [0, -80]
     }
   });
+
+  // Crea el marcador
   var icon = new LeafIcon({ iconUrl: '/assets/icons/marker.png' })
   var marker = L.marker([lat, lon], {icon: icon}).addTo(map);
   return marker;
