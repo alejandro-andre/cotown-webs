ff2d8a1f2c7a45f4a3c701e2f2d232999f96c5d0
Alejandro André <alejandro.andre@ciberexperis.es>
2023-06-21 19:08:19 +0200

    Services DB change

--- desarrollo/src/data/services.js
+++ desarrollo/src/data/services.js
@@ -2,7 +2,7 @@ module.exports = async (config) => {
 
   const gql = require('./graphql');
   const QUERY = `
-  query data {
+  query data ($id: Int!) {
     data: Building_Building_service_typeList (
       orderBy: [{ attribute: Name }]
       where: { Published: { EQ: true } }
@@ -12,8 +12,13 @@ module.exports = async (config) => {
       Name_en
       Description
       Description_en
-      Icon {
-        name
+      Icons: Media_service_typeListViaService_type_id (
+        joinType: INNER
+        where: { Segment_id: { EQ: $id } }
+      ) {
+        id
+        Segment_id
+        Icon { name }
       }
     }
   }`;
