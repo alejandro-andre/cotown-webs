bedb0b024371a50e9b3cd768359501111e689dad
Alejandro André <alejandro.andre@ciberexperis.es>
2023-07-17 11:02:03 +0200

    Fix strange siteid bug

--- desarrollo/run.js
+++ desarrollo/run.js
@@ -1,7 +1,7 @@
 const prices = require('./src/data/residences.js');
 
 (async () => {
-  const config = {'site':'vanguard', 'siteid':1};
+  const config = {'site': 'vanguard', 'siteid': 1};
   const p = await prices(config);
   console.log(JSON.stringify(p, null, 2));
 })();
