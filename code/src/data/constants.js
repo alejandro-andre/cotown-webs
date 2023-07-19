016a2991b5d8d46cbd14ef7dd25b7e424ba29818
Alejandro André <alejandro.andre@ciberexperis.es>
2023-07-19 16:58:02 +0200

    To pro

--- code/src/data/constants.js
+++ code/src/data/constants.js
@@ -1,6 +1,8 @@
 module.exports = Object.freeze({
-  BACK: "https://dev.cotown.ciber.es/api/v1",
-  SERVER: "https://experis.flows.ninja",
+  //BACK: "https://dev.cotown.ciber.es/api/v1",
+  //SERVER: "https://experis.flows.ninja",
+  BACK: "https://back.cotown.com/api/v1",
+  SERVER: "https://core.cotown.com",
   LOGIN:  "mutation{login(username:\"modelsadmin\",password:\"Ciber$2022\")}",
   HEADER: { "Content-Type": "application/json" }
 });
