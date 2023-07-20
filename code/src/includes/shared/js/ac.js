329b83939893acd3d95d16a91aad7ad28f00e109
Alejandro André <alejandro.andre@ciberexperis.es>
2023-07-20 19:32:21 +0200

    Forms v0.4

---
+++ code/src/includes/shared/js/ac.js
@@ -0,0 +1,35 @@
+
+// Post contact
+function post(event) {
+  
+  // Prevent default submit
+  event.preventDefault();
+
+  // Spinner
+  document.getElementById('overlay').style.display = 'flex';
+
+  // Request
+  var xhr = new XMLHttpRequest();
+
+  // Error, remove spinner
+  xhr.onerror = function() {
+    document.getElementById('overlay').style.display = 'none';
+  }
+
+  // Ok, remove spinner, show ok message
+  xhr.onload = function() {
+    document.getElementById('overlay').style.display = 'none';
+    if (xhr.status == 200) {
+      document.getElementById('div-ok').style.display = 'block';
+      document.getElementById('div-send').style.display = 'none';
+    } else {
+      document.getElementById('div-ok').style.display = 'block';
+      document.getElementById('div-send').style.display = 'none';
+    };
+  }
+
+  // Post data
+  formData = new FormData(event.target);
+  xhr.open('POST', "https://back.cotown.com/api/v1/form", true);
+  xhr.send(formData);
+}
