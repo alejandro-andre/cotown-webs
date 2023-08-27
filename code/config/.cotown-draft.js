a6f215c01d527fecfc195db1ee57348f3a223abd
Alejandro André <alejandro.andre@ciberexperis.es>
2023-08-27 18:12:38 +0200

    Fix

--- code/config/.cotown-draft.js
+++ code/config/.cotown-draft.js
@@ -16,7 +16,7 @@ module.exports = function (eleventyConfig) {
       input: "src",
       includes: "includes",
       data: "data",
-      output: "sites/cotown"
+      output: "sites/cotown-draft"
     }
   };
 };
