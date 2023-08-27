a7dfa10ea8b898ecce1a597271dac64440f96193
Alejandro André <alejandro.andre@ciberexperis.es>
2023-08-27 17:51:28 +0200

    Gen parametric

--- gen.sh
+++ gen.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/cotown/www/webs
 git pull
-#docker restart web
-docker exec web bash config/publish.sh vanguard
+docker restart web
+docker exec web bash config/$1.sh $2
 docker logs -f web
