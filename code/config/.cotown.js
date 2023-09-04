ff1c0c27264c05c079a15aaf9d935ab8e48631c5
Alejandro André <alejandro.andre@ciberexperis.es>
2023-09-04 16:14:43 +0200

    Fix

--- code/config/.cotown.js
+++ code/config/.cotown.js
@@ -7,7 +7,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({"src/includes/shared/css": eleventyConfig.root + "/assets/css"});   // remove
   eleventyConfig.addPassthroughCopy({"src/includes/vanguard/css": eleventyConfig.root + "/assets/css"}); // remove
   eleventyConfig.addPassthroughCopy({"src/assets/shared": eleventyConfig.root + "/assets"});
-  eleventyConfig.addPassthroughCopy({"src/assets/vanguard": eleventyConfig.root + "/assets"});
+  eleventyConfig.addPassthroughCopy({"src/assets/cotown": eleventyConfig.root + "/assets"});
   eleventyConfig.addGlobalData("root", eleventyConfig.root);
   eleventyConfig.addGlobalData("site", "cotown");
   eleventyConfig.addGlobalData("siteid", 2);
