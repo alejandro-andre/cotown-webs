b523e21c8532ee0913c8a794caa9932c460b5e38
Alejandro André <alejandro.andre@ciberexperis.es>
2023-11-26 14:38:09 +0100

    More style fixes... again

--- code/src/includes/shared/js/book.js
+++ code/src/includes/shared/js/book.js
@@ -2,20 +2,24 @@
 // /booking/3?lang=en&segment=1&book_building_id=1&book_flat_type_id=1&book_place_type_id=100&book_checkin=2023-11-01&book_checkout=2024-03-31
 function go_book(type) {
     radio = document.querySelector('input[name="radio-' + type + '"]:checked');
-    flat  = radio.getAttribute('data-flat');
-    place = radio.getAttribute('data-place');
-    sessionStorage.setItem('city', '{{building.District.Location.Name}}');
-    sessionStorage.setItem('building', '{{building.Name}}');
-    sessionStorage.setItem('book_city_id', '{{building.District.Location.id}}');
-    sessionStorage.setItem('book_building_id', '{{building.id}}');
-    sessionStorage.setItem('book_type', type);
-    //location.href = '/booking/3?'
-    //  + 'lang={{L}}&segment={{siteid}}&book_building_id={{building.id}}'
-    //  + '&book_flat_type_id=' + flat 
-    //  + '&book_place_type_id=' + place 
-    //  + '&book_checkin=' + sessionStorage.getItem('book_checkin') 
-    //  + '&book_checkout=' + sessionStorage.getItem('book_checkout');
-    location.href = "{{root}}{{langs[L].folder}}/{{langs[L].pages['book'].url}}.html";
+    if (radio) {
+      try {
+        flat  = radio.getAttribute('data-flat');
+        place = radio.getAttribute('data-place');
+      } catch {}
+      sessionStorage.setItem('city', '{{building.District.Location.Name}}');
+      sessionStorage.setItem('building', '{{building.Name}}');
+      sessionStorage.setItem('book_city_id', '{{building.District.Location.id}}');
+      sessionStorage.setItem('book_building_id', '{{building.id}}');
+      sessionStorage.setItem('book_type', type);
+      //location.href = '/booking/3?'
+      //  + 'lang={{L}}&segment={{siteid}}&book_building_id={{building.id}}'
+      //  + '&book_flat_type_id=' + flat 
+      //  + '&book_place_type_id=' + place 
+      //  + '&book_checkin=' + sessionStorage.getItem('book_checkin') 
+      //  + '&book_checkout=' + sessionStorage.getItem('book_checkout');
+      location.href = "{{root}}{{langs[L].folder}}/{{langs[L].pages['book'].url}}.html";
+    }
 }
 
 // Button VISIT
