f57d6aae4dd6562e08ec5670726a79a23ca58606
Alejandro André <alejandro.andre@ciberexperis.es>
2023-11-28 12:14:50 +0100

    Fix

--- code/config/draft.sh
+++ code/config/draft.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 find .cache -type f -mmin +120 -exec rm {} \;
-npx @11ty/eleventy --config=config/.$1-draft.js --debug
+export DEBUG="Eleventy*"
+npx @11ty/eleventy --config=config/.$1-draft.js
 echo 'Done!'
