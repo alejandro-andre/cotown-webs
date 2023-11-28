4ce9ec6073393f38344fe2cdc83cfde387f58e9b
Alejandro André <alejandro.andre@ciberexperis.es>
2023-11-28 12:19:16 +0100

    Debug v2

--- code/config/publish.sh
+++ code/config/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find .cache -type f -mmin +120 -exec rm {} \;
+find code/.cache -type f -mmin +120 -exec rm {} \;
 export DEBUG="Eleventy*"
 npx @11ty/eleventy --config=config/.$1.js
 rsync -rcni /app/sites/$1-publish/ /app/sites/$1/ | cut -d" " -f2 | while read -r line; do
