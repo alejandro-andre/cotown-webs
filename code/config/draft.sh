28a8e6ec77ddd01db65b9a1eb33dad69674ed668
Alejandro André <alejandro.andre@ciberexperis.es>
2023-11-28 12:46:47 +0100

    Fix .cache

--- code/config/draft.sh
+++ code/config/draft.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find code/.cache -type f -mmin +120 -exec rm {} \;
+find /app/.cache -type f -mmin +120 -exec rm {} \;
 export DEBUG="Eleventy*"
 npx @11ty/eleventy --config=config/.$1-draft.js
 echo 'Done!'
--- code/config/publish.sh
+++ code/config/publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-find code/.cache -type f -mmin +120 -exec rm {} \;
+find /app/.cache -type f -mmin +120 -exec rm {} \;
 export DEBUG="Eleventy*"
 npx @11ty/eleventy --config=config/.$1.js
 rsync -rcni /app/sites/$1-publish/ /app/sites/$1/ | cut -d" " -f2 | while read -r line; do
