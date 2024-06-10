3e7549c5404f0b4edaeeda6bbffdcb0f06fb308f
aandre <alejandro.andre@experis.es>
2024-06-10 12:59:13 +0200

    Scripts jsmin

--- code/config/publish.sh
+++ code/config/publish.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 find /app/.cache -type f -mmin +120 -exec rm {} \;
 npx @11ty/eleventy --config=config/.$1.js
-rsync -rcni /app/sites/$1-publish/ /app/sites/$1/ | cut -d" " -f2 | while read -r line; do
+rsync -rcni --delete /app/sites/$1-publish/ /app/sites/$1/ | cut -d" " -f2 | while read -r line; do
   echo /app/sites/$1-publish/$line
-  rsync -ai /app/sites/$1-publish/$line /app/sites/$1/$line
+  rsync -ai --detete /app/sites/$1-publish/$line /app/sites/$1/$line
 done
 echo 'Done!'
