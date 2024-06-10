b34bc75141d4b2033338a5fe5ac08f976949de4a
aandre <alejandro.andre@experis.es>
2024-06-10 12:39:44 +0200

    Fix

---
+++ code/src/data/locations.js
@@ -0,0 +1,36 @@
+module.exports = async (config) => {
+
+  const gql = require('./graphql');
+  const QUERY = `
+query data ($id: Int) {
+    data: Geo_LocationList (
+      orderBy: [{ attribute: Name }]
+      where: { Published: { EQ: true } }
+    ) {
+      id
+      Name
+      Name_en
+      Texts: Location_textListViaLocation_id (
+        joinType: INNER
+        where: { Segment_id: { EQ: $id } }
+      ) {
+          Description
+          Description_en
+          Details
+          Details_en
+      }
+      Images: Media_locationListViaLocation_id (
+        joinType: INNER
+        orderBy: [{ attribute: Order }]
+        where: { Segment_id: { EQ: $id } }
+      ) {
+        id
+        Order
+        Name
+        Image_type
+      }
+    }
+  }`;
+  const data = await gql(QUERY, config, 'locations');
+  return data.data;
+};
