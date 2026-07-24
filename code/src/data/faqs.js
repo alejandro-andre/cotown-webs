115a9f82f3db7cab719aa92e12905fac78d19e45
alejandro-andre <alejandro.andre@oimbra.tech>
2026-07-24 11:56:06 +0200

    FAQs by Building

--- code/src/data/faqs.js
+++ code/src/data/faqs.js
@@ -11,6 +11,7 @@ module.exports = async (config) => {
       Name
       Name_en
       Location_id
+      Building_id
       Questions: Faq_questionListViaTopic_id (
         orderBy: [{attribute: Order, direction:ASC, nullsGo: FIRST}]
       ) {
